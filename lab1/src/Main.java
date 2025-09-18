0f9c9ace6948f9791f4832261224696c8669f9d9
Dania <139234410+d-kuzia@users.noreply.github.com>
2025-09-18 16:46:30 +0300

    lab 1 is done

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -1,5 +1,67 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
+    public static String[] filterShorterThanAvg(String[] input) {
+        if (input == null || input.length == 0) {
+            return new String[0];
+        }
+
+        double avgLength = calculateAverageLength(input);
+        ArrayList<String> result = new ArrayList<>();
+
+        for (String str : input) {
+            if (str.length() < avgLength) {
+                result.add(str);
+            }
+        }
+
+        return result.toArray(new String[0]);
+    }
+
+    public static String[] filterLongerThanAvg(String[] input) {
+        if (input == null || input.length == 0) {
+            return new String[0];
+        }
+
+        double avgLength = calculateAverageLength(input);
+        ArrayList<String> result = new ArrayList<>();
+
+        for (String str : input) {
+            if (str.length() > avgLength) {
+                result.add(str);
+            }
+        }
+
+        return result.toArray(new String[0]);
+    }
+
+    private static double calculateAverageLength(String[] input) {
+        int totalLength = 0;
+        for (String str : input) {
+            totalLength += str.length();
+        }
+        return (double) totalLength / input.length;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] testArr = {"pipi", "bobi", "gamble", "Vitalik1", "Pokemon", "Sorry", "nah"};
+        System.out.println("Array: " +  Arrays.toString(testArr));
+
+        double avg = calculateAverageLength(testArr);
+        System.out.println("Average length: " + avg);
+
+        String[] shorter = filterShorterThanAvg(testArr);
+        System.out.println("Shorter length: " + Arrays.toString(shorter));
+
+        String[] longer = filterLongerThanAvg(testArr);
+        System.out.println("Longer length: " + Arrays.toString(longer));
+
+        // empty
+        String[] empty = {};
+        System.out.println("Empty length: " + Arrays.toString(empty));
+        System.out.println("Shorter length: " + Arrays.toString(filterShorterThanAvg(empty)));
+        System.out.println("Longer length: " + Arrays.toString(filterLongerThanAvg(empty)));
+
     }
 }
